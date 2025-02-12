fdc348fa5e6349c0be81e15379e150acc4aaae26
Frostic21 <145382864+Frostic21@users.noreply.github.com>
2025-02-12 08:46:09 -0800

    Done!

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,18 +1,89 @@
-module.exports.viewALl = function(req, res, next) {
-    const restaurants =[ {
-        id: 1,
-        name: 'Pizza Hut',
-        image: 'https://dynl.mktgcdn.com/p/d9AXTJEWMZ156q11dLLVRHsmufNu0K-ng4JYb_4WwRI/1900x1427.jpg',
-        rating : 2,
-        category : 'Italian',
-        description : 'asldkfhasldf fjadskjj asdda fdss as asldkfhasldf fjadskjj asdda fdss as asldkfhasldf fjadskjj asdda fdss as'
-    },
-    {   id: 2,
-        name: 'Not Pizza Hut',
-        image: 'https://dynl.mktgcdn.com/p/d9AXTJEWMZ156q11dLLVRHsmufNu0K-ng4JYb_4WwRI/1900x1427.jpg',
-        rating : 4,
-        category : 'Italian',
-        description : 'asldkfhasldf fjadskjj asdda fdss as asldkfhasldf fjadskjj asdda fdss as asldkfhasldf fjadskjj asdda fdss as'
-    }];
-    res.render('index', {restaurants});
+const {Restaurant} = require('../models')
+const categories = ['Italian', 'Fast Food', ];
+
+module.exports.viewAll = async function(req, res, next) {
+    let searchCategories = ['All'];
+    for(let i = 0; i<categories.length; i++) {
+        searchCategories.push(categories[i]);
+    }
+    let restaurants;
+    let searchCategory = req.query.category || 'All';
+    let searchRandom = req.query.random || false;
+    if (searchCategory==='All'){
+        restaurants = await Restaurant.findAll();
+    } else {
+        restaurants = await Restaurant.findAll({
+            where: {
+                category: searchCategory
+            }
+        });
+    }
+    if (restaurants.length > 0) {
+        let randomIndex = getRandomInt(restaurants.length);
+        restaurants = [restaurants[randomIndex]];
+    }
+    res.render('index', {restaurants, categories:searchCategories, searchCategories, searchRandom});
+}
+
+module.exports.renderEditForm = async function(req, res, next) {
+    const restaurant = await Restaurant.findByPk(
+        req.params.id
+    );
+    res.render('edit', {restaurant, categories});
+}
+
+module.exports.updateRestaurant = async function(req, res) {
+    await Restaurant.update(
+        {
+            name: req.body.name,
+            category: req.body.category,
+            rating: req.body.rating,
+            image: req.body.image,
+            description: req.body.description
+        },
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+        });
+    res.redirect('/');
+}
+
+module.exports.deleteRestaurant = async function(req, res) {
+    await Restaurant.destroy(
+        {
+            where:
+            {
+                id: req.params.id
+            }
+        });
+    res.redirect('/');
+}
+
+module.exports.renderAddForm = function(req, res) {
+    const restaurant = {
+        name: "",
+        description: "",
+        rating: 1,
+        image: "",
+        category: categories[0],
+    };
+    res.render('add', {restaurant, categories});
+}
+
+module.exports.addRestaurant = async function(req, res) {
+    await Restaurant.create(
+        {
+            name: req.body.name,
+            category: req.body.category,
+            rating: req.body.rating,
+            image: req.body.image,
+            description: req.body.description
+    });
+    res.redirect('/');
+}
+
+function getRandomInt(max){
+    return Math.floor(Math.random() * max);
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ var router = express.Router();
 const restaurantController = require('../controllers/restaurantController');
 
 /* GET home page. */
-router.get('/', restaurantController.viewALl);
+router.get('/', restaurantController.viewAll);
+router.get('/edit/:id', restaurantController.renderEditForm);
+router.post('/edit/:id', restaurantController.updateRestaurant);
+router.get('/delete/:id', restaurantController.deleteRestaurant);
+router.get('/add', restaurantController.renderAddForm);
+router.post('/add', restaurantController.addRestaurant);
 
 module.exports = router;
